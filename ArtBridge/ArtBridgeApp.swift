7255993e5efee70be627eb7cf7da74d58c245eeb
gogochang <okpo2581@gmail.com>
2023-01-28 16:59:11 +0900

    Initial Commit

---
+++ ArtBridge/ArtBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtBridgeApp.swift
+//  ArtBridge
+//
+//  Created by 김창규 on 2023/01/28.
+//
+
+import SwiftUI
+
+@main
+struct ArtBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
