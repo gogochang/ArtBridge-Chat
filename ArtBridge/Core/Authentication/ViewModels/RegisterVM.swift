d7aae7d3625cc3ea93dac984ea1add9b86527aa1
gogochang <okpo2581@gmail.com>
2023-04-05 20:44:54 +0900

    feat(Registration) : 회원가입에서 유저이름과 이메일 중복체크 기능추가
    RegistVM에서
    1. Enum타입의 통한 체크리스트 추가
    2. Firestore의 "users"의 정보와 TextFied의 문자열을 비교하여 경과를 Colmbine Publisher로 전송.
    3. 결과에따라 UI로 표시
    TextFiled마다 valid한 결과를 "&&"논리연산자로 여러개 사용할 때 에러로 인해 코드 표현방법을 단순화 ( && -> if a,b,c )

--- ArtBridge/Core/Authentication/ViewModels/RegisterVM.swift
+++ ArtBridge/Core/Authentication/ViewModels/RegisterVM.swift
@@ -8,9 +8,11 @@
 import Foundation
 import Alamofire
 import Combine
+import Firebase
 
 class RegisterVM: ObservableObject {
     let service = UserService()
+    let db = Firestore.firestore()
 
     var subscription = Set<AnyCancellable>()
 
@@ -39,16 +41,30 @@ class RegisterVM: ObservableObject {
                 
         isUserNameValidPublisher
             .receive(on: RunLoop.main)
-            .map { valid in
-                valid ? "" : "닉네임이 짧습니다. 3글자 이상"
+            .map { nameCheck in
+                switch (nameCheck) {
+                case .short:
+                    return "닉네임이 짧습니다. 3글자 이상"
+                case .duplicate:
+                    return "이미 존재하는 닉네임입니다."
+                default:
+                    return ""
+                }
             }
             .assign(to: \.userNameMessage, on: self)
             .store(in: &cancellables)
         
         isEmailValidPublisher
             .receive(on: RunLoop.main)
-            .map { valid in
-                valid ? "" : "이메일 형식이 올바르지 않습니다. example@mail.com"
+            .map { mailCheck in
+                switch (mailCheck) {
+                case .format:
+                    return "이메일 형식이 올바르지 않습니다. example@mail.com"
+                case .duplicate:
+                    return "이미 존재하는 이메일입니다."
+                default:
+                    return ""
+                }
             }
             .assign(to: \.emailMessage, on: self)
             .store(in: &cancellables)
@@ -98,23 +114,117 @@ class RegisterVM: ObservableObject {
     }
     
     //MARK: - Name Valid
-    private var isUserNameValidPublisher: AnyPublisher<Bool, Never> {
+    enum NameCheck {
+        case valid
+        case duplicate
+        case short
+    }
+
+    //Name Strength
+    private var isNameShortPublisher: AnyPublisher<Bool, Never> {
         $userNameInput
             .debounce(for: 0.8, scheduler: RunLoop.main)
             .removeDuplicates()
             .map { input in
-                return input.count >= 3
+                return input.count < 3
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    // Name Duplicate func
+    private func checkDuplicateName(name: String) -> AnyPublisher<Bool, Never> {
+        return Future<Bool, Never> { promise in
+            self.db.collection("users").whereField("username", isEqualTo: name).getDocuments { snapshot, _ in
+                if snapshot?.count ?? 0 > 0 {
+                    promise(.success(true))
+                } else {
+                    promise(.success(false))
+                }
+            }
+        }.eraseToAnyPublisher()
+    }
+    
+    // Name Duplicate
+    private var isNameDuplicatePublisher: AnyPublisher<Bool, Never> {
+        $userNameInput
+            .debounce(for: 0.8, scheduler: RunLoop.main)
+            .removeDuplicates()
+            .flatMap { input -> AnyPublisher<Bool, Never> in
+                return self.checkDuplicateName(name: input)
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    // Name Valid
+    private var isUserNameValidPublisher: AnyPublisher<NameCheck, Never> {
+        Publishers.CombineLatest(isNameShortPublisher, isNameDuplicatePublisher)
+            .map { nameIsShort, nameIsDuplicate in
+                if(nameIsShort) {
+                    return .short
+                } else if (nameIsDuplicate) {
+                    return .duplicate
+                } else {
+                    return .valid
+                }
             }
             .eraseToAnyPublisher()
     }
     
     //MARK: - Email Valid
-    private var isEmailValidPublisher: AnyPublisher<Bool, Never> {
+    enum EmailCheck {
+        case valid
+        case duplicate
+        case format
+    }
+    
+    // Email format
+    private var isEmailFormatPublisher: AnyPublisher<Bool, Never> {
         $emailInput
             .debounce(for: 0.8, scheduler: RunLoop.main)
             .removeDuplicates()
             .map { input in
                 return (input.range(of: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}", options: .regularExpression) != nil )
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    // Email Duplicate func
+    private func checkDuplicateEmail(email: String) -> AnyPublisher<Bool,Never> {
+        return Future<Bool, Never> { promise in
+            self.db.collection("users").whereField("email", isEqualTo: email).getDocuments { snapshot, _ in
+                if snapshot?.count ?? 0 > 0 {
+                    print("changgyu1 \(email)")
+                    promise(.success(true))
+                } else {
+                    print("changgyu2 \(email)")
+                    promise(.success(false))
+                }
+            }
+        }.eraseToAnyPublisher()
+    }
+    
+    // Email Duplicate
+    private var isEmailDuplicatePublisher: AnyPublisher<Bool, Never> {
+        $emailInput
+            .debounce(for: 0.8, scheduler: RunLoop.main)
+            .removeDuplicates()
+            .flatMap { input -> AnyPublisher<Bool, Never> in
+                return self.checkDuplicateEmail(email: input)
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    // Email Valid
+    private var isEmailValidPublisher: AnyPublisher<EmailCheck, Never> {
+        Publishers.CombineLatest(isEmailFormatPublisher, isEmailDuplicatePublisher)
+            .map { emailIsFormat, emailIsDuplicate in
+                if(!emailIsFormat) {
+                    return .format
+                } else if (emailIsDuplicate) {
+                    return .duplicate
+                } else {
+                    return .valid
+                }
             }
             .eraseToAnyPublisher()
     }
@@ -180,7 +290,12 @@ class RegisterVM: ObservableObject {
     private var isAllValidPublisher: AnyPublisher<Bool, Never> {
         Publishers.CombineLatest3(isUserNameValidPublisher, isEmailValidPublisher, isPasswordVaildPublisher)
             .map { userNameIsValid, emailIsValid, passwordIsValid in
-                return userNameIsValid && emailIsValid && (passwordIsValid == .valid)
+                if (userNameIsValid == .valid), (emailIsValid == .valid), (passwordIsValid == .valid) {
+                    return true
+                } else {
+                    return false
+                }
+//                return true
             }
             .eraseToAnyPublisher()
     }
